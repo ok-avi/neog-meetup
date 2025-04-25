ac0bc9eefd721c9f36c1b9d5ff1ebcb601f9cece
ok-avi <avi11574kumar@gmail.com>
2025-04-25 16:40:39 +0530

    Create: loading component

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading(){
+    return(
+        <div className="d-flex align-items-center justify-content-center" style={{height:"70vh"}}>
+          <p className="text-success fw-medium fs-1 ">Loading...</p>  
+        </div>
+    )
+}
